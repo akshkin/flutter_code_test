54b67f9371f7df0d9ae689f4c184d1fbc28e66f4
akshkin <akshaya.kini12@gmail.com>
2024-10-07 14:33:53 +0200

    Create StockDataService and display data in StockDataVisualization page

--- lib/pages/stock_price_data_visualization.dart
+++ lib/pages/stock_price_data_visualization.dart
@@ -1,15 +1,164 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_code_test/constants/theme.dart';
+import 'package:flutter_code_test/service/stock_price_service.dart';
+import 'package:intl/intl.dart';
 
 class StockPriceDataVisualization extends StatelessWidget {
   const StockPriceDataVisualization({super.key});
 
+  Future<List<Map<String, dynamic>>> fetchStockData() async {
+    try {
+      final data = await StockDataService.fetchStockData('AAPL');
+      return data;
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  Widget bottomTitleWidgets(
+      double value, TitleMeta meta, List<Map<String, dynamic>> stockData) {
+    const style = TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.bold,
+    );
+    String text = "";
+
+    if (value.toInt() >= 0 && value.toInt() < stockData.length) {
+      // display data on X-axis in the format (HH:mm)
+      final time =
+          DateTime.fromMillisecondsSinceEpoch(stockData[value.toInt()]["t"]);
+      final formattedTime = DateFormat('HH:mm').format(time);
+      return SideTitleWidget(
+        axisSide: meta.axisSide,
+        space: 8,
+        child: Text(formattedTime, style: style),
+      );
+    }
+
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 8,
+      child: Text(text, style: style),
+    );
+  }
+
+  Widget leftTitleWidgets(
+      double value, TitleMeta meta, List<Map<String, dynamic>> stockData) {
+    const style = TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.bold,
+    );
+    String text = '';
+
+    if (stockData.isNotEmpty &&
+        value.toInt() >= 0 &&
+        value.toInt() < stockData.length) {
+      // extract the closing price from the stock data
+      final closingPrice = stockData[value.toInt()]["c"];
+      if (closingPrice != null) {
+        text = closingPrice
+            .toString(); // Convert the price to a string for display
+      }
+    }
+
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 8,
+      child: Text(text, style: style),
+    );
+  }
+
+  double getMinY(List<double> closePrices) {
+    return closePrices.reduce((a, b) => a < b ? a : b);
+  }
+
+  double getMaxY(List<double> closePrices) {
+    return closePrices.reduce((a, b) => a > b ? a : b);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text("Stock Price Data"),
-        ),
-        body: Center());
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Stock Price Data"),
+      ),
+      body: FutureBuilder(
+        future: fetchStockData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                snapshot.error.toString(),
+                style: const TextStyle(
+                  color: ThemeColors.errorColor,
+                ),
+              ),
+            );
+          }
+
+          final stockData = snapshot.data!;
+          final List<double> closePrices =
+              stockData.map((item) => (item["c"] as num).toDouble()).toList();
+          final minY = getMinY(closePrices);
+          final maxY = getMaxY(closePrices);
+
+          return Center(
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: LineChart(
+                  LineChartData(
+                    minY: minY,
+                    maxY: maxY,
+                    lineBarsData: [
+                      LineChartBarData(
+                        color: ThemeColors.accentColor,
+                        spots: stockData.asMap().entries.map((e) {
+                          return FlSpot(
+                            e.key.toDouble(),
+                            e.value["c"].toDouble(),
+                          );
+                        }).toList(),
+                        isCurved: true,
+                      ),
+                    ],
+                    titlesData: FlTitlesData(
+                      leftTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                          showTitles: true,
+                          reservedSize: 50,
+                          getTitlesWidget: (value, meta) =>
+                              leftTitleWidgets(value, meta, stockData),
+                        ),
+                      ),
+                      topTitles: const AxisTitles(
+                        sideTitles: SideTitles(showTitles: false),
+                      ),
+                      rightTitles: const AxisTitles(
+                        sideTitles:
+                            SideTitles(showTitles: false, reservedSize: 10),
+                      ),
+                      bottomTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                          showTitles: true,
+                          getTitlesWidget: (value, meta) =>
+                              bottomTitleWidgets(value, meta, stockData),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
---
+++ lib/service/stock_price_service.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_code_test/constants/secrets.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:intl/intl.dart';
+
+class StockDataService {
+  static Future<List<Map<String, dynamic>>> fetchStockData(
+      String ticker) async {
+    // Get today's date without time (hours, minutes, seconds)
+    final today = DateTime.now();
+    final todaysDate = DateFormat('yyyy-MM-dd').format(today);
+
+    final url =
+        'https://api.polygon.io/v2/aggs/ticker/$ticker/range/1/minute/2023-02-09/2023-02-09?adjusted=true&sort=asc&apiKey=${Secrets.polygonAPIKey}';
+    final response = await http.get(Uri.parse(url));
+    Map<String, dynamic> data = jsonDecode(response.body);
+
+    if (data["status"] == "OK") {
+      // Check if 'results' exists and is a list
+      if (data.containsKey("results") && data["results"] is List) {
+        // json first parses as dynamic
+        final List<dynamic> results = data["results"];
+
+        // explicitly cast
+        List<Map<String, dynamic>> castResults =
+            results.map((item) => item as Map<String, dynamic>).toList();
+
+        // Return the properly cast list of maps
+        return castResults;
+      } else {
+        throw Exception('No results found in the response.');
+      }
+    } else {
+      throw Exception('Failed to load stock data');
+    }
+  }
+}
