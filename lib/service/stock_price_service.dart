d5e08e33ddec362ba7396213cddd2d0c3438f1a1
akshkin <akshaya.kini12@gmail.com>
2024-10-14 22:40:45 +0200

    Modify stock data service to dynamically get data for latest day

--- lib/service/stock_price_service.dart
+++ lib/service/stock_price_service.dart
@@ -1,44 +1,50 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter_code_test/constants/dates.dart';
 import 'package:flutter_code_test/constants/secrets.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
 import 'package:intl/intl.dart';
 
 class StockDataService {
   static Future<List<Map<String, dynamic>>> fetchStockData(
       String ticker) async {
-    // Get today's date without time (hours, minutes, seconds)
-    final today = DateTime.now();
-    final todaysDate = DateFormat('yyyy-MM-dd').format(today);
+    DateTime currentDate = DateTime.now();
+    bool isDataAvailable = false;
 
-    final url =
-        'https://api.polygon.io/v2/aggs/ticker/$ticker/range/1/minute/2023-02-09/2023-02-09?adjusted=true&sort=asc&apiKey=${Secrets.polygonAPIKey}';
+    while (!isDataAvailable) {
+      String formattedCurrentDate =
+          DateFormat('yyyy-MM-dd').format(currentDate);
+      try {
+        final url =
+            'https://api.polygon.io/v2/aggs/ticker/$ticker/range/1/minute/$formattedCurrentDate/$formattedCurrentDate?adjusted=true&sort=asc&apiKey=${Secrets.polygonAPIKey}';
+        final response = await http.get(Uri.parse(url));
+        Map<String, dynamic> data = jsonDecode(response.body);
 
-    try {
-      final response = await http.get(Uri.parse(url));
-      Map<String, dynamic> data = jsonDecode(response.body);
+        // free plan does not show data for today's date (API returns "NOT_AUTHORIZED"), check for previous dates until data is found.
+        // data is available for previous day but the resultsCount is 0, check for when the resultsCount is more than 0
+        if (data["status"] == "NOT_AUTHORIZED" || data["resultsCount"] == 0) {
+          currentDate = currentDate.subtract(const Duration(days: 1));
+        } else if (data["status"] == "OK") {
+          DateConstants.latestDate = currentDate;
+          // Check if 'results' exists and is a list
+          if (data.containsKey("results") && data["results"] is List) {
+            // json first parses as dynamic
+            final List<dynamic> results = data["results"];
 
-      if (data["status"] == "OK") {
-        // Check if 'results' exists and is a list
-        if (data.containsKey("results") && data["results"] is List) {
-          // json first parses as dynamic
-          final List<dynamic> results = data["results"];
+            // explicitly cast
+            List<Map<String, dynamic>> castResults =
+                results.map((item) => item as Map<String, dynamic>).toList();
 
-          // explicitly cast
-          List<Map<String, dynamic>> castResults =
-              results.map((item) => item as Map<String, dynamic>).toList();
-
-          // Return the properly cast list of maps
-          return castResults;
+            // Return the properly cast list of maps
+            return castResults;
+          } else {
+            throw Exception('No results found in the response.');
+          }
         } else {
-          throw Exception('No results found in the response.');
+          throw Exception('Failed to load stock data');
         }
-      } else {
-        throw Exception('Failed to load stock data');
+      } catch (e) {
+        throw Exception(e);
       }
-    } catch (e) {
-      throw Exception('Failed to load stock data: $e');
     }
   }
 }
