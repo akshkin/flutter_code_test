2b87577e8ec942564570fc79c66078b08730bae7
akshkin <akshaya.kini12@gmail.com>
2024-10-14 22:36:03 +0200

    Add new 'stockName' variable

--- lib/environment.dart
+++ lib/environment.dart
@@ -2,6 +2,7 @@ import 'package:flutter_code_test/constants/constants.dart';
 
 abstract class AppEnvironment {
   static late String stockTicker;
+  static late String stockName;
   static late Environment _environment;
 
   static Environment get environment => _environment;
@@ -11,9 +12,11 @@ abstract class AppEnvironment {
     switch (environment) {
       case Environment.dev:
         stockTicker = "AAPL";
+        stockName = "Apple";
         break;
       case Environment.prod:
         stockTicker = "NVDA";
+        stockName = "Nvidia";
         break;
     }
   }
